050432048585ceccc75569af410f1c89b5a1bff8
aldsanms <73821252+aldsanms@users.noreply.github.com>
2022-09-27 08:51:59 +0200

    Create File

---
+++ sharedCode/shiny/1stTest/app.r
@@ -0,0 +1,38 @@
+
+
+
+library(shiny)
+library(r2d3)
+
+AllData <- read.csv(file = "data/data.csv",stringsAsFactors = FALSE,
+                    strip.white = TRUE,
+                    sep = ";")
+
+
+
+ui <- fluidPage(
+  verbatimTextOutput("selected"),
+  d3Output("d3")
+)
+
+
+
+
+server <- function(input, output) {
+  output$d3 <- renderD3({
+    
+    
+    r2d3(
+      data=AllData, 
+      script = "js/test01.js"
+    )
+  
+    
+  })
+  output$selected <- renderText({
+    bar_number <- as.numeric(req(input$bar_clicked))
+    if (bar_number > 0) cos(bar_number)
+  })
+}
+
+shinyApp(ui, server)
