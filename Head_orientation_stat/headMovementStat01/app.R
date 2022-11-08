7f6ec4263c708aeb08e9164f97adcfa7c7c57439
aldsanms <aldryck.sanchez@viacesi.fr>
2022-11-08 10:26:43 +0100

    Adding headMovementStat01 project

---
+++ Head_orientation_stat/headMovementStat01/app.R
@@ -0,0 +1,53 @@
+
+library(shiny)
+library(r2d3)
+library(tidyverse)
+
+
+
+AllData <- read.csv(file = "data/log_Sample.csv",stringsAsFactors = FALSE,
+                         strip.white = TRUE,                    sep = ";")
+
+
+AllMole <- read.csv(file = "data/AllMole.csv",stringsAsFactors = FALSE,
+                     strip.white = TRUE,
+                    sep = ",")
+
+
+
+
+data_to_json <- function(data) {
+  jsonlite::toJSON(data, dataframe = "rows", auto_unbox = FALSE, rownames = TRUE)
+} 
+
+data <- list("am" = AllMole,"ad" = AllData)
+
+ui <- fluidPage(
+  verbatimTextOutput("selected"),
+  
+  
+  d3Output("d3")
+  
+)
+
+
+
+
+server <- function(input, output) {
+  output$d3 <- renderD3({
+    
+    
+    r2d3(
+      data= data_to_json(data),
+      script = "js/main.js"
+    )
+  
+    
+  })
+  output$selected <- renderText({
+    bar_number <- as.numeric(req(input$bar_clicked))
+    if (bar_number > 0) cos(bar_number)
+  })
+}
+
+shinyApp(ui, server)
