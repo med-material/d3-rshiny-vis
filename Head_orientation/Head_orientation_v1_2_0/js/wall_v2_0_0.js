55939462937149d869beed69f40720c29908433d
aldsanms <aldryck.sanchez@viacesi.fr>
2022-11-25 15:12:46 +0100

    add Head_orientation_v1_2_0

---
+++ Head_orientation/Head_orientation_v1_2_0/js/wall_v2_0_0.js
@@ -0,0 +1,165 @@
+//////////////////////////////////////////////////
+/*                                              */
+/*      CREATE Aalborg University               */
+/*      aldsanms                                */
+/*      nov-24-2022                             */
+/*      wall_v2_0_0.js                          */
+//////////////////////////////////////////////////
+
+//Contains the various global functions for the operation of the program.
+
+var wall = {
+  
+  
+    wallSettings : {
+        height : 250,//px // height of the graph
+        width : 750,//px // width of the graph
+        
+        marginTop : 10,//px // margin of the graph compared to the top of the window
+        marginLeft : 25,//px // margin of the graph compared to the left of the window
+        
+        
+        yScale : {}, // value to pixel for the y conversion obj
+        xScale : {}, // value to pixel for the x conversion obj
+        
+        
+        xMin : -5,//minimum x value for the graph
+        xMax : 5,//maximum x value for the graph
+        
+        yMin : -3,//minimum y value for the graph
+        yMax : 3,//maximum y value for the graph
+        
+        
+        pointSize : 20,//px
+        pointColor : "#D6D6D6", // default color of the moles
+        
+        laserSize : 10,//px
+        laserColor : "red", // default color of the laser
+        
+    },
+    
+    test : function(){
+        return 1;
+    },
+    
+    addGraphMoles : function() {
+    
+        var xStep = ((data.mi[1]["x_extrem"]*2)/((data.mi[0]["n_col_row"])-1))
+        var yStep = ((data.mi[1]["y_extrem"]*2)/((data.mi[1]["n_col_row"])-1))
+
+        for(var c = 0;c < (data.mi[0]["n_col_row"]); c++){
+          for(var r = 0;r < (data.mi[1]["n_col_row"]); r++){
+
+            if(
+              ((c == 0) && (r == 0))||
+              ((c == 0) && (r == (data.mi[1]["n_col_row"])-1))||
+              ((c == (data.mi[0]["n_col_row"]-1)) && (r == 0))||
+              ((c == (data.mi[0]["n_col_row"]-1)) && (r == (data.mi[1]["n_col_row"])-1))
+            ){
+               
+            }else{
+              if((r+1) < 10){
+                DataArrays.idMoles.push((c+1)+"0"+(r+1));
+              }else{
+                DataArrays.idMoles.push((c+1)+""+(r+1));
+              }
+              
+              DataArrays.objMoles.push(
+                    utils.addPointOnSvg(graphWall,wall.wallSettings.pointSize,wall.wallSettings.xScale(data.mi[0]["x_extrem"] + xStep*c),wall.wallSettings.yScale(data.mi[0]["y_extrem"] + yStep*r),wall.wallSettings.pointColor)
+               );
+            }
+          }
+        }
+        console.log(DataArrays.idMoles)
+    },
+    
+    changeMolesState : function(){
+    
+        if(new Date(DataArrays.indexDate) >= new Date(data.aed[DataArrays.indexMole]['Timestamp'])){
+            while(new Date(DataArrays.indexDate) >= new Date(data.aed[DataArrays.indexMole]['Timestamp'])){
+            
+                var thisMole = DataArrays.objMoles[DataArrays.idMoles.indexOf(parseInt(data.aed[DataArrays.indexMole]['MoleId']))];
+                
+                switch(data.aed[DataArrays.indexMole]['Event']){
+                    
+                    case "Mole Spawned":
+                        utils.changeFillElement(thisMole,"green",1000);
+                        DataArrays.indexMole++;
+                        break;
+                    
+                    case "Fake Mole Spawned":
+                        utils.changeFillElement(thisMole,"#FFFB00",1000);
+                        DataArrays.indexMole++;
+                        break;
+                    
+                    case "Mole Expired":
+                        utils.changeFillElement(thisMole, wall.wallSettings.pointColor,1000);
+                        DataArrays.indexMole++
+                        break;
+                    
+                    case "Fake Mole Expired":
+                        utils.changeFillElement(thisMole, wall.wallSettings.pointColor,1000);
+                        DataArrays.indexMole++
+                        break;
+                    
+                    case "Mole Hit":
+                        utils.changeFillElement(thisMole,"orange",0);
+                        utils.changeFillElement(thisMole, wall.wallSettings.pointColor,1000);
+                        DataArrays.indexMole++
+                        break;
+                    
+                    case "Fake Mole Hit":
+                        utils.changeFillElement(thisMole,"red",0);
+                        utils.changeFillElement(thisMole, wall.wallSettings.pointColor,1000);
+                        DataArrays.indexMole++
+                        break;
+                    
+                    default:
+                        DataArrays.indexMole++
+                        break;
+                
+                }
+            }
+        }
+    },
+    
+    
+    resetMoles : function(){
+        DataArrays.objMoles.forEach((element,index) => { 
+            DataArrays.objMoles[index]
+            .attr('fill',  wall.wallSettings.pointColor);
+        });
+    },
+        
+    addLaser : function(){
+        DataArrays.objLaser = utils.addPointOnSvg(graphWall, wall.wallSettings.laserSize,-100,-100,"red");
+    },
+    
+    getLaserCoordPx : function(i){
+        var coord = [0,0];
+        
+        if(data.ad[i]['RightControllerLaserPosWorldX'] != "NULL")coord[0] =  wall.wallSettings.xScale(data.ad[i]['RightControllerLaserPosWorldX']);
+        if(data.ad[i]['RightControllerLaserPosWorldY'] != "NULL")coord[1] =  wall.wallSettings.yScale(data.ad[i]['RightControllerLaserPosWorldY']);
+        
+        return coord;
+    },
+    
+    changeLaserLocation : function(){
+    
+        if(new Date(DataArrays.indexDate) >= new Date(data.ad[DataArrays.indexLaser]['Timestamp'])){
+            while(new Date(DataArrays.indexDate) >= new Date(data.ad[DataArrays.indexLaser]['Timestamp'])){
+            
+                DataArrays.objLaser.transition()
+                    .duration(14)
+                    .attr('cx', function(d) { return wall.getLaserCoordPx(DataArrays.indexLaser )[0]; })
+                    .attr('cy', function(d) { return wall.getLaserCoordPx(DataArrays.indexLaser )[1]; });
+                    
+                DataArrays.indexLaser++
+                
+            }
+        }
+    }
+
+};
+
+
